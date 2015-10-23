aef18c0b9ed0411b9c0070f099c3aac335431105
James Wright <jcwright04@gmail.com>
2015-10-23 16:44:02 -0700

    added header and footer directives

---
+++ js/directives/directiveFooter.js
@@ -0,0 +1,16 @@
+'use strict';
+//Directive
+myApp.directive('footerSection', function () {
+    return {
+        restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
+        scope: {
+            //@ reads the attribute value, = provides two-way binding, & works with functions
+            title: '@'         },
+        templateUrl: 'pages/footer-section.html',
+        controller: function ($scope) {
+	      $scope.name = 'jwright04';
+
+	    },
+        link: function ($scope, element, attrs) { } //DOM manipulation
+    }
+});
---
+++ js/directives/directiveNavigation.js
@@ -0,0 +1,16 @@
+'use strict';
+//Directive
+myApp.directive('navSection', function () {
+    return {
+        restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
+        scope: {
+            //@ reads the attribute value, = provides two-way binding, & works with functions
+            title: '@'         },
+        templateUrl: 'pages/nav-section.html',
+        controller: function ($scope) {
+	      $scope.name = 'jwright04';
+
+	    },
+        link: function ($scope, element, attrs) { } //DOM manipulation
+    }
+});
